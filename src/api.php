c97a0f4363b9cf14f4c62eb9b7ce5221f496b549
AhoyRipper Caretaker <caretaker@ahoyripper.com>
2026-06-03 17:54:38 +0000

    chore: incremental improvement [260603-1752]

--- src/api.php
+++ src/api.php
@@ -1344,7 +1344,7 @@ switch ($action) {
                 }
             }
             http_response_code($err_status);
-            $resp = ['error' => $parsed['error']];
+            $resp = ['error' => $parsed['error'], 'request_id' => $request_id];
             if (!empty($parsed['error_code'])) {
                 $resp['error_code'] = $parsed['error_code'];
             }
