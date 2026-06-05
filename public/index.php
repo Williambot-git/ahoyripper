2a3746e918e1c45be6661aa09e59c9ce867610b8
AhoyRipper Caretaker <caretaker@ahoyripper.com>
2026-06-05 02:12:44 +0000

    chore: incremental improvement [250605-0210]

--- public/index.php
+++ public/index.php
@@ -119,7 +119,7 @@ header('X-Content-Type-Options: nosniff');
 <body>
 
 <!-- Navigation -->
-<nav class="ahoy-nav">
+<nav class="ahoy-nav" aria-label="Main navigation">
   <a href="/" class="ahoy-nav-logo">
     <svg class="ahoy-nav-icon" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
       <rect width="28" height="28" rx="6" fill="#3b82f6"/>
