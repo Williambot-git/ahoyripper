72a02b10d8ddd4742ea81ad0e7b3deb5dfda02bf
AhoyRipper Caretaker <caretaker@ahoyripper.com>
2026-08-07 11:30:32 +0000

    chore: incremental improvement [260807-1124]

--- src/api.php
+++ src/api.php
@@ -1185,6 +1185,7 @@ $validation = function(string $action) use($request_id, $sendDailyLimitHeaders) 
             'error' => 'URL is too long. Please paste a shorter link.',
             'error_code' => 'INVALID_URL',
             'request_id' => $request_id,
+            'source_url' => $url,
             'yt_dlp_version' => $GLOBALS['__ytdlp_version'] ?? null,
         ], JSON_INVALID_UTF8_SUBSTITUTE);
         return false;
