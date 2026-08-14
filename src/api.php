35d8008985565575ffa490d659f1c4b94274417e
AhoyRipper Caretaker <caretaker@ahoyripper.com>
2026-08-14 21:02:05 +0000

    chore: incremental improvement [260814-2100]

--- src/api.php
+++ src/api.php
@@ -443,6 +443,7 @@ if (in_array($action, $internal_actions, true)) {
         'app_version' => AHOYRIPPER_VERSION,
         'php_version' => PHP_VERSION,
         'api_version' => AHOYRIPPER_VERSION,
+        'yt_dlp_version' => $GLOBALS['__ytdlp_version'] ?? null,
         'source_url' => 'https://ahoyripper.com',
     ]);
     exit;
