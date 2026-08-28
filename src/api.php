c196f2fb33f0d3ea2f429c1d8ba9a26c122a7afb
AhoyRipper Caretaker <caretaker@ahoyripper.com>
2026-08-28 10:22:25 +0000

    chore: incremental improvement [260828-1018]

--- src/api.php
+++ src/api.php
@@ -5549,7 +5549,7 @@ switch ($action) {
                     '--dump-json',
                     '--no-playlist',
                     '--skip-download',
-                    '--progress-template', "",
+                    '--progress-template', 'false',
                     '--retries', '3',
                     '--extractor-retries', '3',
                     '--socket-timeout', (string)max(1, floor(HEALTH_PROBE_TIMEOUT / 2)),
