3add0698337e4b6bbfbec844997baebf401d73ec
AhoyRipper Caretaker <caretaker@ahoyripper>
2026-09-14 05:05:16 +0000

    chore: incremental improvement [260914-0510]

--- src/api.php
+++ src/api.php
@@ -337,6 +337,7 @@ if ($blocked) {
         // continues. Add them here explicitly so FORBIDDEN_ORIGIN responses are
         // fully consistent with all other API error responses.
         header('Content-Security-Policy: default-src \'self\'; script-src \'self\'; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com; font-src \'self\' https://fonts.googleapis.com https://fonts.gstatic.com; img-src \'self\' data: https://i.ytimg.com https://*.tikcdn.com https://*.tiktokcdn.com https://pbs.twimg.com https://*.twimg.com https://*.sndcdn.com https://*.vimeocdn.com https://*.instagram.com https://*.fbcdn.net https://v16.tiktokcdn.com https://v26.tiktokcdn.com https://*.tiktok.com https://vxtiktok.com https://*.mediaJx.com https://fonts.googleapis.com; connect-src \'self\'; upgrade-insecure-requests; frame-ancestors \'none\'; frame-src \'none\'; worker-src \'self\'; object-src \'none\'; base-uri \'self\'; form-action \'self\'; report-to csp-report;');
+        header('X-Robots-Tag: noindex, noai, noimage, noydir');
         header('Reporting-Endpoints: csp-report="/csp-report"');
         header('Report-To: {"group":"csp-report","max_age":86400,"endpoints":[{"url":"/csp-report"}]}');
         // Cache-Control: no-store is set globally at line 163, but add it here too
