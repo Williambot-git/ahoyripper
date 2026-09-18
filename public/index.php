7713ce98d1f164620023eb57313cb730732ac331
AhoyRipper Caretaker <caretaker@ahoyripper>
2026-09-18 21:10:07 +0000

    chore: incremental improvement [260918-2112]

--- public/index.php
+++ public/index.php
@@ -97,7 +97,7 @@ header('X-Request-ID: ' . $page_request_id);
 header_remove('X-Powered-By');
 ?>
 <!DOCTYPE html>
-<html lang="en-US" class="no-js">
+<html lang="en-US" dir="ltr" class="no-js">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
